a80a2860bb2d9491f917548a6465d06f7120de40
Ben Swerdlow <benswerdlow1005@gmail.com>
2020-10-16 13:44:42 -0400

    FakeGmail test complet

--- selenium/src/test/java/selenium/test/google/FakeGmailSetup.java
+++ selenium/src/test/java/selenium/test/google/FakeGmailSetup.java
@@ -1,13 +1,12 @@
 package selenium.test.google;
 
+import static org.junit.Assert.fail;
+
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
@@ -15,12 +14,14 @@ import org.apache.commons.csv.CSVRecord;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import selenium.DriverUtils;
@@ -71,46 +72,107 @@ class FakeGmailSetup {
 		}
 		return we;
 	}
-
-	@Test
-	void test() {
-		for (CSVRecord rec : csvData) {
-			boolean progressed = false;
-			for (int i=1; i<rec.size()-2; i++) {
-				String[] header = headers.get(i).split(" ");
-				WebElement we = findElementBySplitHeader(header);
-				if (we == null) {
-					if (!progressed) {
-						driver.findElement(By.tagName("form")).submit();
-						progressed = true;
-					}
-					WebDriverWait wait = new WebDriverWait(driver, 5);
-					wait.until(ExpectedConditions.presenceOfElementLocated(By.id("phoneNumberId")));
-					we = findElementBySplitHeader(header);
-					if (we == null) {
-						System.out.println("Test failed");
-						System.out.println("Could not find field associated with "+headers.get(i));
-					} else {
-						we.sendKeys(rec.get(i));
-						driver.findElements(By.tagName("button")).get(1).click();
-						System.out.println("Test passed");
-					}
-				} else {
-					we.sendKeys(rec.get(i));
-				}
-			}
-//			WebElement firstName = driver.findElement(By.id("firstName"));
-//			firstName.sendKeys(rec.get("id firstName"));
-//			WebElement lastName = driver.findElement(By.id("lastName"));
-//			lastName.sendKeys(rec.get("id lastName"));
-//			WebElement username = driver.findElement(By.id("username"));
-//			username.sendKeys(rec.get("id username"));
-//			WebElement password = driver.findElement(By.name("Passwd"));
-//			password.sendKeys(rec.get("name Passwd"));
-//			WebElement confPassword = driver.findElement(By.name("ConfirmPasswd"));
-//			confPassword.sendKeys(rec.get("name ConfirmPasswd"));
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/gmailTestCases.csv", numLinesToSkip = 1)
+	void csvTest(int testNum, String firstName, String lastName, String username, String password, String confirmPassword, String phoneNumberId) {
+		System.out.println(testNum+" "+firstName+" "+lastName+" "+username+" "+password+" "+confirmPassword+" "+phoneNumberId);
+		driver.findElement(By.id("firstName")).sendKeys(firstName);
+		driver.findElement(By.id("lastName")).sendKeys(lastName);
+		driver.findElement(By.id("username")).sendKeys(username);
+		driver.findElement(By.name("Passwd")).sendKeys(password);
+		driver.findElement(By.name("ConfirmPasswd")).sendKeys(confirmPassword);
+		driver.findElement(By.id("accountDetailsNext")).click();
+		
+		WebDriverWait wait = new WebDriverWait(driver, 3);
+		try {
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.id("phoneNumberId")));
+		} catch (TimeoutException e) {
+			List<WebElement> errorWebElements = driver.findElements(By.cssSelector("div[id$='Error']"));
+			System.out.println("Test "+testNum+" failed");
+			System.out.println("Error Message:");
+			StringBuilder sb = new StringBuilder();
+			errorWebElements.forEach(we -> 
+//				System.out.println(we.getTagName()+", "+we.getAttribute("id")+", "+we.getText()+", "+we.getAttribute("value")+", "+we.getAttribute("innerText")+", "+we.getAttribute("innerHTML"))
+				{System.out.println(we.getText()); sb.append(we.getText());}
+			);
+			fail(sb.toString());
 		}
+		driver.findElement(By.id("phoneNumberId")).sendKeys(phoneNumberId);
+		try {
+			wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("div[id$='Error']")));
+		} catch (TimeoutException e) {
+			driver.findElements(By.tagName("button")).get(1).click();
+			System.out.println("Test "+testNum+" passed");
+			return;
+		}
+		List<WebElement> errorWebElements = driver.findElements(By.cssSelector("div[id$='Error']"));
+		System.out.println("Test "+testNum+" failed");
+		System.out.println("Error Message:");
+		errorWebElements.forEach(we -> System.out.println(we.getText()));
+		fail();
 	}
+
+//	@Test
+//	void test() {
+//		for (CSVRecord rec : csvData) {
+//			boolean progressed = false;
+//			for (int i=1; i<rec.size()-2; i++) {
+//				String[] header = headers.get(i).split(" ");
+//				System.out.println(Arrays.toString(header));
+//				WebElement we = findElementBySplitHeader(header);
+//				if (we == null) {
+//					if (!progressed) {
+//						driver.findElement(By.id("accountDetailsNext")).click();
+//						progressed = true;
+//						WebDriverWait wait = new WebDriverWait(driver, 3);
+//						wait.until(
+//								ExpectedConditions.or(
+//										ExpectedConditions.presenceOfElementLocated(By.id("phoneNumberId")),
+//										ExpectedConditions.presenceOfElementLocated(By.cssSelector("div[id$='Error']"))
+//								)
+//						);
+//					}
+//					we = findElementBySplitHeader(header);
+//					if (we == null) {
+//						System.out.println("Test failed");
+//						if (driver.findElements(By.cssSelector("div[id$='Error']")).size() > 0) {
+//							WebElement error = driver.findElement(By.cssSelector("div[id$='Error']"));
+//							System.out.println("Error Message: "+error.getTagName()+" - "+error.getAttribute("innerHTML"));
+//						} else {
+//							System.out.println("Could not find field associated with "+headers.get(i));
+//						}
+//					} else {
+//						we.clear();
+//						we.sendKeys(rec.get(i));
+//						driver.findElements(By.tagName("button")).get(1).click();
+//						WebDriverWait wait = new WebDriverWait(driver, 5);
+//						wait.until(ExpectedConditions.presenceOfElementLocated(By.id("accountDetailsNext")));
+//						System.out.println("Test passed");
+//						progressed = false;
+//					}
+//				} else {
+//					System.out.println(we.getTagName()+" "+we.getAttribute("id"));
+//					System.out.println(we.getAttribute("value"));
+//					Keys[] backSpaces = new Keys[we.getAttribute("value").length()];
+//					Arrays.fill(backSpaces, Keys.BACK_SPACE);
+//					we.sendKeys(Keys.chord(backSpaces));
+//					System.out.println(we.getAttribute("value"));
+//					we.sendKeys(rec.get(i));
+//				}
+//			}
+////			WebElement firstName = driver.findElement(By.id("firstName"));
+////			firstName.sendKeys(rec.get("id firstName"));
+////			WebElement lastName = driver.findElement(By.id("lastName"));
+////			lastName.sendKeys(rec.get("id lastName"));
+////			WebElement username = driver.findElement(By.id("username"));
+////			username.sendKeys(rec.get("id username"));
+////			WebElement password = driver.findElement(By.name("Passwd"));
+////			password.sendKeys(rec.get("name Passwd"));
+////			WebElement confPassword = driver.findElement(By.name("ConfirmPasswd"));
+////			confPassword.sendKeys(rec.get("name ConfirmPasswd"));
+//		}
+//	}
 	
 	@AfterAll
 	public static void shutdown() {
